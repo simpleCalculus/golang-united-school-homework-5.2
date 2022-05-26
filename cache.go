e536e7d457b8434e0cf4269dce5ddd008cca9b1f
simpleCalculus <60369311+simpleCalculus@users.noreply.github.com>
2022-05-26 14:54:10 +0500

    Add files via upload

--- cache.go
+++ cache.go
@@ -1,23 +1,58 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type myTimeValue struct {
+	value    string
+	deadline time.Time
+}
 
 type Cache struct {
+	simpleMap map[string]string
+	timeMap   map[string]myTimeValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	sm := make(map[string]string)
+	tm := make(map[string]myTimeValue)
+	return Cache{sm, tm}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Put(key, value string) {
+	c.simpleMap[key] = value
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.timeMap[key] = myTimeValue{value, deadline}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.simpleMap[key]
+	if ok {
+		return value, ok
+	}
+
+	timeValue, ok := c.timeMap[key]
+	if !ok {
+		return "", ok
+	}
+	if timeValue.deadline.After(time.Now()) {
+		return timeValue.value, ok
+	}
+	return "", false
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0)
+	for k := range c.simpleMap {
+		keys = append(keys, k)
+	}
+	for k, v := range c.timeMap {
+		if v.deadline.After(time.Now()) {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
